9b1af726ed8137bfe90f233492e2857b2a640127
paul <paulandre.vallat@gmail.com>
2025-05-08 21:37:54 +0200

    initial2 commit

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+// pages/about.js
+import Image from 'next/image';
+export default function AboutPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>QR Code pour vallat.andre@outlook.com</h2>
+      <p>
+       <img src="qrcode.png" 
+       alt="QR Code Email"
+       width={330}
+       height={330}/>
+       </p>
+    </div>
+  );
+}
